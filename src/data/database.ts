c37891e215ee0eddede695cf7a4d34aac649fd56
Andrea <f.andrea.marco@hotmail.it>
2020-08-07 16:31:11 +0200

    .

--- src/data/database.ts
+++ src/data/database.ts
@@ -3,7 +3,7 @@ import { account } from "./structure";
 
 export class Database {
 
-    private client = new MongoClient("mongodb+srv://admin:admin@cluster0.vlznh.mongodb.net/accounts_list?retryWrites=true&w=majority", { useUnifiedTopology: true });
+    private client = new MongoClient("mongodb+srv://admin:admin@cluster0.vlznh.mongodb.net/kebotdb?retryWrites=true&w=majority", { useUnifiedTopology: true });
     private isConnected_ = false;
     public get isConnected() { return this.isConnected_ }
     private set isConnectedSet(value: boolean) { this.isConnected_ = value };
@@ -32,7 +32,7 @@ export class Database {
     public async addUser(account : any){
         if(this.isConnected){
             await new Promise((res, rej)=>{
-                this.client.db("accounts_list").collection("userlist").insertOne(account, (err,stat)=>{
+                this.client.db("kebotdb").collection("accounts").insertOne(account, (err,stat)=>{
                     if(err) rej(err);
                     else res(stat);
                 });
